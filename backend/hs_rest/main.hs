7f5b798bca2f50b5a430393383d203f46220ca8d
Mutestock <doublenegative@protonmail.com>
2023-02-19 19:53:25 +0100

    starting js

--- backend/hs_rest/main.hs
+++
@@ -1,5 +0,0 @@
--- This is really just a placeholder
--- I'm not sure whether or not it'll ever be relevant
--- But I'm interested in Haskell
--- Especially since I don't know any functional programming as of yet
--- We'll see if I ever get around to implementing this feature.
