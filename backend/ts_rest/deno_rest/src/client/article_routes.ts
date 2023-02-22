adc583fa6466e62bc100f2a2f09e3f243c84ef46
Mutestock <doublenegative@protonmail.com>
2023-02-22 22:10:43 +0100

    deno start

---
+++ backend/ts_rest/deno_rest/src/client/article_routes.ts
@@ -0,0 +1,7 @@
+import * as grpc from "npm:@grpc/grpc-js@1.8.10";
+import * as protoLoader from "npm:@grpc/proto-loader@0.7.5";
+
+/*
+import * as protoLoader from "@grpc/proto-loader";
+import * as grpc from "@grpc/grpc-js";
+*/
