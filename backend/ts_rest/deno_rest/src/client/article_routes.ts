1404f85bfedc80a1c7e64ee3ff9c93bd463c3828
Mutestock <doublenegative@protonmail.com>
2023-02-25 14:17:57 +0100

    scope decreased

--- backend/ts_rest/deno_rest/src/client/article_routes.ts
+++
@@ -1,7 +0,0 @@
-import * as grpc from "npm:@grpc/grpc-js@1.8.10";
-import * as protoLoader from "npm:@grpc/proto-loader@0.7.5";
-
-/*
-import * as protoLoader from "@grpc/proto-loader";
-import * as grpc from "@grpc/grpc-js";
-*/
